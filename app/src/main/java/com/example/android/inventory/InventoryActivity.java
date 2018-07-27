706f9e165b84d6c45fd9a6533e0aa876b364257d
angelynaliem <angelynaliem@gmail.com>
2018-07-27 00:12:29 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/inventory/InventoryActivity.java
@@ -0,0 +1,103 @@
+package com.example.android.inventory;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.android.inventory.data.InventoryContract.InventoryEntry;
+import com.example.android.inventory.data.InventoryDbHelper;
+
+public class InventoryActivity extends AppCompatActivity {
+
+    private InventoryDbHelper mDbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+
+        mDbHelper = new InventoryDbHelper(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        displayDatabaseInfo();
+
+    }
+
+    private void displayDatabaseInfo() {
+
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        String[] projection = {
+                InventoryEntry._ID,
+                InventoryEntry.COLUMN_INVENTORY_PRODUCT_NAME,
+                InventoryEntry.COLUMN_INVENTORY_PRICE,
+                InventoryEntry.COLUMN_INVENTORY_QUANTITY,
+                InventoryEntry.COLUMN_INVENTORY_SUPPLIER_NAME,
+                InventoryEntry.COLUMN_INVENTORY_SUPPLIER_PHONE_NUMBER };
+
+        Cursor cursor = db.query(
+                InventoryEntry.TABLE_NAME,
+                projection,
+                InventoryEntry.COLUMN_INVENTORY_PRODUCT_NAME,
+               null,
+                null,
+                null,
+                null);
+
+        TextView displayView = findViewById(R.id.text_view_inventory);
+
+        try {
+            displayView.setText("The inventory table contains " + cursor.getCount() + " products.\n\n");
+            displayView.append(InventoryEntry._ID + " - " +
+                    InventoryEntry.COLUMN_INVENTORY_PRODUCT_NAME + " - " +
+                    InventoryEntry.COLUMN_INVENTORY_PRICE + " - " +
+                    InventoryEntry.COLUMN_INVENTORY_QUANTITY + " _ " +
+                    InventoryEntry.COLUMN_INVENTORY_SUPPLIER_NAME + " - " +
+                    InventoryEntry.COLUMN_INVENTORY_SUPPLIER_PHONE_NUMBER + "\n");
+
+            int idColumnIndex = cursor.getColumnIndex(InventoryEntry._ID);
+            int nameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVENTORY_PRODUCT_NAME);
+            int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVENTORY_PRICE);
+            int quantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVENTORY_QUANTITY);
+            int supplierNameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVENTORY_SUPPLIER_NAME);
+            int supplierPhoneNumberColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVENTORY_SUPPLIER_PHONE_NUMBER);
+
+            while (cursor.moveToNext()) {
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentName = cursor.getString(nameColumnIndex);
+                int currentPrice = cursor.getInt(priceColumnIndex);
+                int currentQuantity = cursor.getInt(quantityColumnIndex);
+                String currentSupplier = cursor.getString(supplierNameColumnIndex);
+                int currentSupplierNumber = cursor.getInt(supplierPhoneNumberColumnIndex);
+
+                displayView.append(("\n" + currentID + " - " +
+                        currentName + " - " +
+                        currentPrice + " - " +
+                        currentQuantity + " - " +
+                        currentSupplier + " - " +
+                        currentSupplierNumber));
+            }
+        } finally {
+            cursor.close();
+
+        }
+
+}
+
+private void insertInventory() {
+
+    SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+    ContentValues values = new ContentValues();
+    values.put(InventoryEntry.COLUMN_INVENTORY_PRODUCT_NAME, "Product A");
+    values.put(InventoryEntry.COLUMN_INVENTORY_PRICE, 0);
+
+    long newRowId = db.insert(InventoryEntry.TABLE_NAME, null, values);
+}
+}
---
+++ app/src/main/java/com/example/android/inventory/data/InventoryContract.java
@@ -0,0 +1,22 @@
+package com.example.android.inventory.data;
+
+import android.provider.BaseColumns;
+
+public final class InventoryContract {
+
+    private InventoryContract() {}
+
+    public static final class InventoryEntry implements BaseColumns {
+
+        public final static String TABLE_NAME = "inventory";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_INVENTORY_PRODUCT_NAME = "name";
+        public final static String COLUMN_INVENTORY_PRICE = "price";
+        public final static String COLUMN_INVENTORY_QUANTITY = "quantity";
+        public final static String COLUMN_INVENTORY_SUPPLIER_NAME = "supplier name";
+        public final static String COLUMN_INVENTORY_SUPPLIER_PHONE_NUMBER = "supplier phone number";
+
+
+    }
+    }
